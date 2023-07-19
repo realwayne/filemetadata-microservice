19e396b87cb578caa8b790169b0c6c97e84805e5
Realwayne Gonzales <201880542@psu.palawan.edu.ph>
2023-07-19 14:22:38 +0800

    added multer and endpoint /api/fileanalyse

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
+const multer = require("multer");
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+
+const upload = multer({ dest: "/uploads" });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +14,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res) {
+  const { originalname, mimetype, size } = req.file;
 
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
